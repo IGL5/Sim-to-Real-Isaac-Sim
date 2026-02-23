53615498bd196f4c5a955e173b94368c11807b81
IGL5 <ikergo55@gmail.com>
2026-02-23 10:45:42 +0100

    refactor: fix reflaction metallic use

--- modules/content.py
+++ modules/content.py
@@ -61,7 +61,7 @@ def load_pbr_materials(stage):
     for i, folder_path in enumerate(material_folders):
         try:
             files = glob.glob(os.path.join(folder_path, "*.*"))
-            found_maps = {"diffuse": None, "normal": None, "roughness": None, "ao": None, "emission": None, "metalness": None}
+            found_maps = {"diffuse": None, "normal": None, "roughness": None, "ao": None, "emission": None, "metallic": None}
             normal_gl = None
             normal = None
             
@@ -75,7 +75,7 @@ def load_pbr_materials(stage):
                     else: normal = f_path
                 elif any(x in name for x in ["ao", "ambientocclusion"]): found_maps["ao"] = f_path
                 elif any(x in name for x in ["emiss", "emit"]): found_maps["emission"] = f_path
-                elif any(x in name for x in ["metal", "met"]): found_maps["metalness"] = f_path
+                elif any(x in name for x in ["metal", "met"]): found_maps["metallic"] = f_path
 
             if not found_maps["diffuse"]: continue
             found_maps["normal"] = normal_gl if normal_gl else normal
@@ -92,8 +92,8 @@ def load_pbr_materials(stage):
             with rep_mat:
                 if found_maps["roughness"]:
                     rep.modify.attribute("inputs:reflectionroughness_texture", found_maps["roughness"])
-                if found_maps["metalness"]:
-                    rep.modify.attribute("inputs:reflectionmetalness_texture", found_maps["metalness"])
+                if found_maps["metallic"]:
+                    rep.modify.attribute("inputs:reflectionmetallic_texture", found_maps["metallic"])
                 if found_maps["normal"]:
                     rep.modify.attribute("inputs:normalmap_texture", found_maps["normal"])
                 if found_maps["ao"]:
